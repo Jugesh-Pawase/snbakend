e3214ea42bac5e7ecf621e57096981cc922e9f68
Jugesh Kisan Pawase <jugeshpawase@gmail.com>
2025-10-07 23:45:39 +0530

    payment gateway

--- config/razorpay.js
+++ config/razorpay.js
@@ -5,6 +5,9 @@ if (!process.env.RAZORPAY_KEY || !process.env.RAZORPAY_SECRET) {
     throw new Error("Razorpay credentials are missing");
 }
 
+// console.log("RAZORPAY_KEY_ID:", process.env.RAZORPAY_KEY);
+// console.log("RAZORPAY_SECRET:", process.env.RAZORPAY_SECRET);
+
 exports.instance = new Razorpay({
     key_id: process.env.RAZORPAY_KEY,
     key_secret: process.env.RAZORPAY_SECRET,
